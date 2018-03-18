92c162b1883f0400973963dd4091460fff88f80b
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2018-03-18 23:08:10 +0300

    Introducing dsl

---
+++ mcdsl.py
@@ -0,0 +1,12 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+
+mc = minecraft.Minecraft.create()
+mc.postToChat("DSL initialized")
+
+def teleport(height):
+  playerPos = mc.player.getPos()
+  mc.player.setPos(playerPos.x, playerPos.y + height, playerPos.z)
+  mc.postToChat("Dont look down")
+  time.sleep(1)
