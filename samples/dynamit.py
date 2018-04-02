552cfa4459cda978625fd4415f3a217d19377aad
legkvla <vladimir.legkunets@gmail.com>
2018-04-02 13:38:50 +0300

    Adding example

---
+++ samples/dynamit.py
@@ -0,0 +1,17 @@
+import mcdsl
+import mcpi.block as block
+c = mcdsl.Context()
+
+c.block(block.TNT)
+c.left(1)
+c.block(block.TNT)
+c.left(1)
+c.block(block.TNT)
+c.left(1)
+c.block(block.TNT)
+c.up(1)
+c.block(block.TNT)
+c.right(1)
+c.block(block.TNT)
+c.right(1)
+c.block(block.TNT)
