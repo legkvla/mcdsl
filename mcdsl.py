dd54c07363abc640ef3cbfc408c22f9a5ddc35b6
legkvla <vladimir.legkunets@gmail.com>
2018-05-29 21:46:50 +0300

    Implementing buildx and buildz

--- mcdsl.py
+++ mcdsl.py
@@ -17,21 +17,25 @@ class Context(object):
 
     def __init__(self):
         self.x = self.playerPos.x
-        self.y = self.playerPos.y
+        self.height = self.playerPos.y
         self.z = self.playerPos.z
 
     def buildx(self, x, height, kind):
-        #self.x += x
-        #self.y += height
-        mc.setBlock(self.x, self.y, self.z, kind)
+        for dx in range(x):
+            for dh in range(height):
+                mc.setBlock(self.x + dx, self.height + dh, self.z, kind)
+        self.x += x
+        self.height += height
 
     def buildz(self, z, height, kind):
-        #self.z += z
-        #self.y += height
-        mc.setBlock(self.x, self.y, self.z, kind)
+        for dz in range(z):
+            for dh in range(height):
+                mc.setBlock(self.x, self.height + dh, self.z + dz, kind)
+        self.z += z
+        self.height += height
 
     def build(self, x, z, height, kind):
         #self.x += x
         #self.z += z
         #self.y += height
-        mc.setBlock(self.x, self.y, self.z, kind)
+        mc.setBlock(self.x, self.height, self.z, kind)
